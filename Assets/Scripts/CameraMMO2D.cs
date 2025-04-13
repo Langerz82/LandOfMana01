64b54340bfc604af2f2cdbc330765327fd792868
Joshua L <Langerz82@users.noreply.github.com>
2025-04-14 09:05:00 +1000

    Initial commit

---
+++ Assets/Scripts/CameraMMO2D.cs
@@ -0,0 +1,165 @@
+// Simple MMO camera that always follows the player.
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+using TilemapRenderer = UnityEngine.Tilemaps.TilemapRenderer;
+
+using Debug = UnityEngine.Debug;
+
+public class CameraMMO2D : MonoBehaviour
+{
+
+    [Header("Target Follow")]
+    public Transform target;
+
+    // the target position can be adjusted by an offset in order to foucs on a
+    // target's head for example
+    public Vector2 offset = Vector2.zero;
+
+    // smooth the camera movement
+    [Header("Dampening")]
+    public float damp = 1f;
+
+    // BEGIN MOD - JL - 8/6/24, 30/6/24
+    [HideInInspector] public TilemapRenderer myCollider;
+    public GameObject cameraBounds;
+
+    // END MOD
+
+    private Camera myCamera;
+
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+        SetCameraBounds(cameraBounds);
+    }
+
+    void LateUpdate()
+    {
+        if (!target) return;
+
+        // calculate goal position
+        Vector2 goal = (Vector2)target.transform.position + offset;
+        //goal.x += offset.x;
+        //goal.y += offset.y;
+
+        // BEGIN MOD - JL - 6/6/24
+        float precision = 64f;
+        float toleranceX = precision / Screen.width;
+        float toleranceY = precision / Screen.height;
+        Vector2 diff = (Vector2)transform.position - (Vector2)goal;
+
+        Vector3 position = new Vector3(goal.x, goal.y, transform.position.z);
+
+        /*if (Math.Abs(diff.x) > toleranceX || Math.Abs(diff.y) > toleranceY)
+        {
+            // interpolate
+            position = Vector2.Lerp(transform.position, goal, Time.deltaTime * damp);
+        }*/
+
+        if (myCollider)
+        {
+            Debug.Log("position:" + position);
+            Vector3 pos = ClampCamera(position);
+            if (pos != Vector3.zero)
+            {
+                position.x = pos.x;
+                position.y = pos.y;
+            }
+        }
+
+        // END MOD
+        // convert to 3D but keep Z to stay in front of 2D plane
+        transform.position = position;
+    }
+
+    public Vector3 ClampCamera(Vector3 position)
+    {
+        Vector2 camSize = new Vector2(
+            myCamera.orthographicSize * myCamera.aspect,
+            myCamera.orthographicSize);
+        Debug.Log("ClampMap - camSize:" + camSize);
+
+        return ClampMapWithOffset(myCollider, camSize, position);
+    }
+
+    public Vector3 ClampMapWithOffset(Vector3 position, Vector2 offset)
+    {
+        return ClampMapWithOffset(myCollider, offset, position);
+    }
+
+    // BEGIN MOD - JL - 6/6/24
+    // Exact Copy from function in CameraMMO2D.
+    protected Vector3 ClampMapWithOffset(TilemapRenderer myCollider, Vector2 offset, Vector3 position)
+    {
+        /*Vector2 mapSize = new Vector2(myCollider.bounds.size.x, myCollider.bounds.size.y);
+        Debug.Log("ClampMap - mapSize:" + mapSize);
+        //Debug.Log("myCollider.offset=" + myCollider.offset);
+        Debug.Log("myCollider.size=" + myCollider.bounds.size);
+        Debug.Log("myCollider.center=" + myCollider.bounds.center);
+        Debug.Log("myCollider.extents=" + myCollider.bounds.extents);
+
+        Bounds bounds = myCollider.bounds;
+        Vector2 boxTopLeft = new Vector2(
+            bounds.center.x - bounds.extents.x,
+            bounds.center.y + bounds.extents.y);
+        Debug.Log("ClampMap - boxTopLeft:" + boxTopLeft);
+
+        Vector2 mapMin = new Vector2(
+            myCollider.bounds.min.x,
+            myCollider.bounds.min.y);
+        Vector2 mapMax = new Vector2(
+            myCollider.bounds.max.x,
+            myCollider.bounds.max.y);*/
+
+        Vector2 clipMin = myCollider.bounds.min;
+        Vector2 clipMax = myCollider.bounds.max;
+        if (offset != Vector2.zero)
+        {
+            //Debug.Log("ClampMap - mapMin:" + mapMin);
+            //Debug.Log("ClampMap - mapMax:" + mapMax);
+
+            clipMin = new Vector2(clipMin.x + offset.x, clipMin.y + offset.y);
+            clipMax = new Vector2(clipMax.x - offset.x, clipMax.y - offset.y);
+            Debug.Log("ClampMap - clipMin:" + clipMin);
+            Debug.Log("ClampMap - clipMax:" + clipMax);
+            Debug.Log("ClampMap - position:" + position);
+        }
+
+        float newX = Mathf.Clamp(position.x, clipMin.x, clipMax.x);
+        float newY = Mathf.Clamp(position.y, clipMin.y, clipMax.y);
+        Debug.Log("ClampMap - new_position:" + (new Vector2(newX, newY)));
+        return new Vector3(newX, newY, position.z);
+    }
+    // END MOD
+
+    // BEGIN MOD - JL - 8/6/24
+    // Switch Camera Position to target immediately.
+    public void MoveCamera(Vector3 newPosition)
+    {
+        Debug.Log("MoveCamera - called:" + newPosition);
+        // convert to 3D but keep Z to stay in front of 2D plane
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+    // END MOD
+
+    // BEGIN MOD - JL - 8/6/24
+    public void SetCameraBounds(GameObject bounds)
+    {
+        if (bounds)
+        {
+            cameraBounds = bounds;
+            myCollider = cameraBounds.transform.GetChild(0).GetChild(0).GetComponent<TilemapRenderer>();
+        }
+        else
+        {
+            cameraBounds = null;
+            myCollider = null;
+        }
+    }
+    // END MOD
+
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEngine;
+
+//using Rigidbody2D = UnityEngine.Physics2DModule.Rigidbody2D;
+using SpriteLibrary = UnityEngine.U2D.Animation.SpriteLibrary;
+using SpriteLibraryAsset = UnityEngine.U2D.Animation.SpriteLibraryAsset;
+
+//using CameraMMO2D;
+
+using Debug = UnityEngine.Debug;
+
+public class Player : MonoBehaviour
+{
+    [HideInInspector] public Vector2 lookDirection;
+    public float speed = 1/16f;
+
+    [HideInInspector] public CameraMMO2D cameraScript;
+    [HideInInspector] public GameObject playerCamera;
+    [HideInInspector] public Rigidbody2D myRigidBody;
+
+    [HideInInspector] public string state;
+
+    protected Animator[] mAnimators = new Animator[2];
+    protected SpriteLibrary[] mySpriteLibs = new SpriteLibrary[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameObject.FindWithTag("MainCamera");
+        if (playerCamera)
+            cameraScript = playerCamera.GetComponent<CameraMMO2D>();
+        else
+            Debug.LogError("MainCamera not found.");
+
+        // BEGIN MOD - JL - 5/6/24 - 12/6/24
+        //mAnimators = new Animator[2];
+        //mySpriteLibs = new SpriteLibrary[2];
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        Transform childTF = transform.GetChild(0).Find("sprites").Find("sprite_body");
+        if (childTF)
+        {
+            Debug.Log("childTF - found");
+            mAnimators[0] = GetComponent<Animator>();
+            if (!mAnimators[0]) mAnimators[0] = childTF.GetComponent<Animator>();
+            if (!mAnimators[0]) Debug.Log("Player - mAnimators[0] not found.");
+            mySpriteLibs[0] = childTF.GetComponent<SpriteLibrary>();
+            if (!mySpriteLibs[0]) Debug.Log("BODY SPRITELIB NOT FOUND.");
+        }
+
+        childTF = transform.GetChild(0).Find("sprites").Find("sprite_weapon");
+        if (childTF)
+        {
+            Debug.Log("childTF2 - found");
+            mAnimators[1] = GetComponent<Animator>();
+            if (!mAnimators[1]) mAnimators[1] = childTF.GetComponent<Animator>();
+            if (!mAnimators[1]) Debug.Log("Player - mAnimators[1] not found.");
+            mySpriteLibs[1] = childTF.GetComponent<SpriteLibrary>();
+            if (!mySpriteLibs[1]) Debug.Log("WEAPON SPRITELIB NOT FOUND.");
+        }
+// END MOD
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = Vector2.zero;
+
+        Vector3 destination = myRigidBody.transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            lookDirection.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            lookDirection.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            lookDirection.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            lookDirection.x = 1;
+        }
+
+        if (lookDirection.x != 0)
+        {
+            destination.x += (lookDirection.x * speed);
+        }
+        if (lookDirection.y != 0)
+        {
+            destination.y += (lookDirection.y * speed);
+        }
+
+// BEGIN MOD - JL - 5/6/24 - 12/6/24
+        //bool isMoving = movement.IsMoving() && state != "CASTING" && !mountControl.IsMounted();
+        bool isMoving = lookDirection != Vector2.zero;
+
+        foreach (Animator mAnimator in mAnimators)
+        {
+            mAnimator.SetBool("MOVING", isMoving);
+            mAnimator.SetBool("CASTING", state == "CASTING");
+            /*foreach (Skill skill in skills.skills)
+                if (skill.level > 0 && !(skill.data is PassiveSkill) && AnimationHasParameter(mAnimator, skill.name))
+                    mAnimator.SetBool(skill.name, skill.CastTimeRemaining() > 0);*/
+            mAnimator.SetBool("STUNNED", state == "STUNNED");
+            mAnimator.SetBool("DEAD", state == "DEAD");
+            mAnimator.SetFloat("LookX", lookDirection.x);
+            mAnimator.SetFloat("LookY", lookDirection.y);
+
+        }
+// END MOD
+
+        destination = clampPlayer(destination);
+
+        //bool hit = Physics2D.Raycast(destination, -Vector2.down, 1, 1 << LayerMask.NameToLayer("Default"));
+
+        //if (!hit)
+            myRigidBody.transform.position = destination;
+    }
+
+    protected Vector3 clampPlayer(Vector3 destination)
+    {
+        Vector3 offset = GetComponent<CircleCollider2D>().bounds.extents;
+        //offset.x /= 2;
+        //offset.y /= 2;
+
+        return cameraScript.ClampMapWithOffset(destination, offset);
+    }
+
+    public bool IsMovementAllowed()
+    {
+        return true;
+    }
+}
