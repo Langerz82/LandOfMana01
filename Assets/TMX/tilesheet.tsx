ce0ea0314aee5628dc5d8b97fd5102d051ab6158
Joshua L <Langerz82@users.noreply.github.com>
2025-04-19 22:52:38 +1000

    correct in tilesheet fix

--- Assets/TMX/tilesheet.tsx
+++ Assets/TMX/tilesheet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="tilesheet" tilewidth="16" tileheight="16" tilecount="5120" columns="20">
+<tileset version="1.2" tiledversion="1.3.4" name="tilesheet" tilewidth="16" tileheight="16" tilecount="1960" columns="20">
  <properties>
   <property name="v" value=""/>
  </properties>
